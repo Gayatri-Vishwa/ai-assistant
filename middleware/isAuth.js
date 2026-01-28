0ef2435dd95f5afc534970ec3e3469c88fe313b3
gayatriDev02 <blossomjasmine02@gmail.com>
2026-01-28 21:13:14 +0530

    trying to solve middleware

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,52 +1,52 @@
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
-// const isAuth=async(req,resp,next)=>{
-// try {
+const isAuth=async(req,resp,next)=>{
+try {
     
-//     // const token=req.cookies.token;
-//     console.log("AUTH COOKIES:", req.cookies);
-//     const token=req.cookies.token;
-//     if(!token){
-//         return resp.status(401).json({message:"Unauthorized"});
-//     }
-//     const decoded=  jwt.verify(token,process.env.JWT_SECRET);
-//     //req.userId = decoded.id;
-//     req.userId = decoded.userId;
-//     next();
-
-//     // ================================
-
-// } catch (error) {
-//     console.error("Authentication error:", error);
-
-//     return resp.status(500).json({message:"Unauthorized"});
-// }
-
-// }
-// export default isAuth;
-
-
-
-import jwt from "jsonwebtoken";
-
-const isAuth = (req, res, next) => {
-  let token = req.cookies?.token;
-
-  if (!token && req.headers.authorization?.startsWith("Bearer ")) {
-    token = req.headers.authorization.split(" ")[1];
-  }
-
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // const token=req.cookies.token;
+    console.log("AUTH COOKIES:", req.cookies);
+    const token=req.cookies.token;
+    if(!token){
+        return resp.status(401).json({message:"Unauthorized"});
+    }
+    const decoded=  jwt.verify(token,process.env.JWT_SECRET);
+    //req.userId = decoded.id;
     req.userId = decoded.userId;
     next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-};
 
+    // ================================
+
+} catch (error) {
+    console.error("Authentication error:", error);
+
+    return resp.status(500).json({message:"Unauthorized"});
+}
+
+}
 export default isAuth;
+
+
+
+// import jwt from "jsonwebtoken";
+
+// const isAuth = (req, res, next) => {
+//   let token = req.cookies?.token;
+
+//   if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+//     token = req.headers.authorization.split(" ")[1];
+//   }
+
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.userId = decoded.userId;
+//     next();
+//   } catch (err) {
+//     return res.status(401).json({ message: "Invalid token" });
+//   }
+// };
+
+// export default isAuth;
