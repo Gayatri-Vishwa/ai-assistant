5c3540897a4603fdd3fd94a7a3aa7646b2e4d246
gayatriDev02 <blossomjasmine02@gmail.com>
2026-01-28 21:08:22 +0530

    trying to solve middleware

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,62 +1,52 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-const isAuth=async(req,resp,next)=>{
-try {
+// const isAuth=async(req,resp,next)=>{
+// try {
     
-    // const token=req.cookies.token;
-    console.log("AUTH COOKIES:", req.cookies);
+//     // const token=req.cookies.token;
+//     console.log("AUTH COOKIES:", req.cookies);
+//     const token=req.cookies.token;
+//     if(!token){
+//         return resp.status(401).json({message:"Unauthorized"});
+//     }
+//     const decoded=  jwt.verify(token,process.env.JWT_SECRET);
+//     //req.userId = decoded.id;
+//     req.userId = decoded.userId;
+//     next();
+
+//     // ================================
+
+// } catch (error) {
+//     console.error("Authentication error:", error);
+
+//     return resp.status(500).json({message:"Unauthorized"});
+// }
+
+// }
+// export default isAuth;
 
 
 
-    const token=req.cookies.token;
-    if(!token){
-        return resp.status(401).json({message:"Unauthorized"});
-    }
-    const decoded=  jwt.verify(token,process.env.JWT_SECRET);
-    //req.userId = decoded.id;
+import jwt from "jsonwebtoken";
+
+const isAuth = (req, res, next) => {
+  let token = req.cookies?.token;
+
+  if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
     next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
 
-    // ================================
-    // 1️⃣ Cookie token
-  //   if (req.cookies?.token) {
-  //     token = req.cookies.token;
-  //   }
-
-  //   // 2️⃣ Header token
-  //   if (!token && req.headers.authorization?.startsWith("Bearer ")) {
-  //     token = req.headers.authorization.split(" ")[1];
-  //   }
-  //  if (!token) {
-  //     return resp.status(401).json({ message: "Unauthorized" });
-  //   }
-
-  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  //   req.userId = decoded.userId;
-
-  //   next();
-// ==================================================
-
-// let token = req.cookies?.token;
-
-// if (!token && req.headers.authorization?.startsWith("Bearer ")) {
-//   token = req.headers.authorization.split(" ")[1];
-// }
-
-// if (!token) {
-//   return resp.status(401).json({ message: "Unauthorized" });
-// }
-
-
-
-} catch (error) {
-    console.error("Authentication error:", error);
-
-    return resp.status(500).json({message:"Unauthorized"});
-}
-
-}
 export default isAuth;
-
-
-
