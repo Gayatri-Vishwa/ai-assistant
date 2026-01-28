9ab12a09cdb8024af7f5dc3c7e834df6b1170ee1
gayatriDev02 <blossomjasmine02@gmail.com>
2026-01-28 18:54:50 +0530

    Starting Auth Complete

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -6,33 +6,33 @@ try {
     const token=req.cookies.token;
 
 
-    // // const token=req.cookies.token;
-    // if(!token){
-    //     return resp.status(401).json({message:"Unauthorized"});
-    // }
-    // const decoded=  jwt.verify(token,process.env.JWT_SECRET);
-    // //req.userId = decoded.id;
-    // req.userId = decoded.userId;
-    // next();
+    // const token=req.cookies.token;
+    if(!token){
+        return resp.status(401).json({message:"Unauthorized"});
+    }
+    const decoded=  jwt.verify(token,process.env.JWT_SECRET);
+    //req.userId = decoded.id;
+    req.userId = decoded.userId;
+    next();
 
     // ================================
     // 1️⃣ Cookie token
-    if (req.cookies?.token) {
-      token = req.cookies.token;
-    }
+  //   if (req.cookies?.token) {
+  //     token = req.cookies.token;
+  //   }
 
-    // 2️⃣ Header token
-    if (!token && req.headers.authorization?.startsWith("Bearer ")) {
-      token = req.headers.authorization.split(" ")[1];
-    }
-   if (!token) {
-      return resp.status(401).json({ message: "Unauthorized" });
-    }
+  //   // 2️⃣ Header token
+  //   if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+  //     token = req.headers.authorization.split(" ")[1];
+  //   }
+  //  if (!token) {
+  //     return resp.status(401).json({ message: "Unauthorized" });
+  //   }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
+  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  //   req.userId = decoded.userId;
 
-    next();
+  //   next();
 // ==================================================
 
 //     const authHeader = req.headers.authorization;
